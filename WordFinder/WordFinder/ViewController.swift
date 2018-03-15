50a816af409f37095884ce20dfd17c6e8f62e2a3
Nic Ollis <nic@oll.is>
2018-03-15 10:15:32 -0400

    initial commit

---
+++ WordFinder/WordFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WordFinder
+//
+//  Created by Nicholas Ollis on 3/15/18.
+//  Copyright © 2018 Nicholas Ollis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
