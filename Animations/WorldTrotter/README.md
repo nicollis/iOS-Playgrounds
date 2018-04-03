04a4a24e38d430a88aa70645fd3a82796f02e065
Nic Ollis <nic@oll.is>
2018-04-02 21:40:49 -0400

    Completed first challenge

---
+++ Animations/WorldTrotter/README.md
@@ -0,0 +1,13 @@
+#  WorldTrotter
+
+Application built in Chapture 1 and 2 of Advance iOS Training
+Topics of study included
+- Custom View Controller Transitions
+- Interactive Transitions
+
+## Challenge 1: Localize SlideTransition
+
+Change UITabBar flow in the same direction as the user interface
+
+**Bonus** I updated the Interactive Transition to also properly flow with the user interface
+
---
+++ Animations/WorldTrotter/WorldTrotter/TabDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  TabDelegate.swift
+//  WorldTrotter
+//
+//  Created by Nicholas Ollis on 3/26/18.
+//  Copyright © 2018 Big Nerd Ranch. All rights reserved.
+//
+
+import UIKit
+
+class TabDelegate: NSObject, UITabBarControllerDelegate {
+    private let slideTransition = SlideTransition()
+    private let slideInteraction = SlideInteraction()
+    
+    var layoutDirection: UIUserInterfaceLayoutDirection = UIApplication.shared.userInterfaceLayoutDirection
+    
+    init(tabBarController: UITabBarController) {
+        super.init()
+        tabBarController.addInteraction(slideInteraction)
+        slideInteraction.tabBarController = tabBarController
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        let fromIndex = tabBarController.viewControllers!.index(of: fromVC)!
+        let toIndex = tabBarController.viewControllers!.index(of: toVC)!
+        
+        if (layoutDirection == .leftToRight) {
+            slideTransition.animationDirection = (fromIndex < toIndex) ? .left : .right
+        } else {
+            slideTransition.animationDirection = (fromIndex < toIndex) ? .right : .left
+        }
+
+        return slideTransition
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+        return slideInteraction.currentlyInteractive ? slideInteraction : nil
+    }
+}
